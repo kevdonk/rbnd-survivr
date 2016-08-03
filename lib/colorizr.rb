f6552d239f4f3bb1e1f7e2eac5a3ec789bf42611
Kevin <kevin.mayo@udacity.com>
2016-08-03 14:04:49 -0700

    Convert @@colors to hash

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,23 +1,19 @@
 class String
-  @@colors = [['red', 31], ['green',32], ['yellow',33], ['blue',34], ['pink',35], ['light_blue',36], ['white',97], ['light_grey',37], ['black',30]]
+  @@colors = {red: 31, green: 32, yellow: 33, blue: 34, pink: 35, light_blue: 36, white: 97, light_grey: 37, black: 30}
   def self.colors
-    returned_colors =  Array.new
-    @@colors.each do |color|
-      returned_colors.push color[0]
-    end
-    return returned_colors
+    @@colors.keys
   end
   def self.create_colors
-    @@colors.each do |color|
-      self.send(:define_method, "#{color[0]}") do
-        puts "\e[#{color[1]}m#{self}\e[0m"
+    self.colors.each do |color|
+      self.send(:define_method, "#{color}") do
+        puts "\e[#{@@colors[color]}m#{self}\e[0m"
       end
     end
   end
   def self.sample_colors
-    @@colors.each do |color|
+    self.colors.each do |color|
       print "This is "
-      "#{color[0]}".send(color[0])
+      "#{color}".send(color)
     end
   end
 end
